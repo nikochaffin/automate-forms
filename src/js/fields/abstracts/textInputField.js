a75bbafbcb2baf8dbfc730bff19d7033eb8d72a4
Niko Chaffin <niko@bixly.com>
2016-12-05 10:28:26 -0800

    Testing for workaround with the lack of specificity for e.keyCode

--- src/js/fields/abstracts/textInputField.js
+++ src/js/fields/abstracts/textInputField.js
@@ -12,16 +12,32 @@ function TextInputField(config) {
   _self.el.addEventListener('blur', function(e){ _self._onFieldBlur.call(_self, e) });
   _self.el.addEventListener('input', function(e){ _self._onFieldInput.call(_self, e) });
   _self.el.addEventListener('keydown', function(e){ _self._onKeyDown.call(_self, e) });
+  _self.el.addEventListener('keypress', function(e){ _self._onKeyPress.call(_self, e) });
   _self._onFieldInput();
 }
 
 TextInputField.prototype._onKeyDown = function(e) {
   var _self = this;
+  // console.log(e);
+  console.log(e.target.value);
+  if (!e.key) {
+    e.target.setAttribute('no-e-key', '');
+  }
+  var char = e.key || String.fromCharCode(e.which);
+  // console.log(e.which, char, String.fromCharCode(e.keyCode), String.fromCharCode(e.charCode), char === "", !!char, char === " ");
+  if (!e.key && ((e.which < 46 && e.which != 32) || (e.which > 111 && e.which <= 123))) {
+    // console.log("Key should be ignored.");
+    return true;
+  }
   if (_self._allowedCharacters) {
-    if (e.key.length == 1 && (!_self._isValidCharacter(e.key) || !_self._checkLimitedCharacter(e.key))) {
+    if (char.length == 1 && (!_self._isValidCharacter(char) || !_self._checkLimitedCharacter(char))) {
       e.preventDefault();
     }
   }
+}
+
+TextInputField.prototype._onKeyPress = function(e) {
+  console.log(e.target.value);
 }
 
 TextInputField.prototype.setAllowedCharacters = function(valid) {
@@ -48,18 +64,27 @@ TextInputField.prototype._checkLimitedCharacter = function(char) {
   if (!_self._limitedCharacters) {
     return true;
   }
-  var charLimit = _self._limitedCharacters[char];
-  if (charLimit && (typeof charLimit == "number" || typeof charLimit.limit == "number")) {
-    var re = new RegExp(charLimit.re, 'g') || new RegExp(char, 'g');
-    var limit = charLimit.limit || charLimit;
-    var uses = _self.value.toString().match(re);
-    return (!uses || uses.length < charLimit);
+  // var charLimit = _self._limitedCharacters[char];
+  // console.log(charLimit);
+  for (c in _self._limitedCharacters) {
+    var charLimit = _self._limitedCharacters[c];
+    if (charLimit && (typeof charLimit == "number" || typeof charLimit.limit == "number")) {
+      var re = new RegExp(charLimit.re, 'g') || new RegExp(char, 'g');
+      if (char.match(re)) {
+        var limit = charLimit.limit || charLimit;
+        var uses = _self.value.toString().match(re);
+        return (!uses || uses.length < charLimit.limit);
+      }
+    }
   }
   return true;
 }
 
 TextInputField.prototype._onFieldInput = function(e) {
   var _self = this;
+  if (e) {
+    console.log(e.target.hasAttribute('no-e-key'));
+  }
   _self.value = _self.valueParse(_self.el.value);
   if (_self.wrapperEl) {
     if (_self.value !== "") {
--- src/js/fields/dateField.js
+++ src/js/fields/dateField.js
@@ -7,20 +7,21 @@ DateField.prototype.constructor = DateField;
 function DateField(config) {
   var _self = this;
   _self.valueParse = function(val) {
-    if (val !== "") {
-      return parseFloat(val);
-    }
-    return "";
+    var d = new Date(val);
+    console.log(d);
+    return val;
   }
   TextInputField.call(_self, config);
-  _self.el.classList.add(_s.prefixClass('field--decimal'));
-  _self.setAllowedCharacters(/[\d\.]/);
+  _self.el.classList.add(_s.prefixClass('field--date'));
+  _self.setAllowedCharacters(/[\d-/ ]/);
   _self.setLimitedCharacters({
-    ".": {
-      re: /\./,
-      limit: 1
+    "separator": {
+      re: /[-/ ]/,
+      limit: 2
     }
   });
+
+  // Allowed pattern: /^\d{0,2}(?!\d)[-/ ]?\d{0,2}?[-/ ]?\d{0,4}?$/
 }
 
 module.exports = DateField;
