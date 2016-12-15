a882ed92df02b07b68e28fc69d439b7cd6790b66
Niko Chaffin <niko@bixly.com>
2016-12-15 13:47:03 -0800

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -66,6 +66,6 @@ gulp.task('serve-gh-pages', ['watch'], function() {
 
   gulp.watch('./*.js').on('change', browserSync.reload);
   gulp.watch('./*.html').on('change', browserSync.reload);
-})
+});
 
 gulp.task('default', ['serve']);
