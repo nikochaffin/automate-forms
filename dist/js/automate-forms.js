6f693965e1ad7f693e953f29a73a968e30be2890
Niko Chaffin <niko@bixly.com>
2016-12-20 16:58:39 -0800

    Updated formDataToObject to properly handle radio buttons and more strictly test before attempting to parseFloat

--- dist/js/automate-forms.js
+++ dist/js/automate-forms.js
@@ -280,13 +280,23 @@
 	  while (!item.done) {
 	    var val;
 	    if (form) {
-	      var input = form.querySelector("input[name=\"" + item.value + "\"]");
-	      val = input.automateValue || input.afValue || input.value;
+	      var inputs = form.querySelectorAll("input[name=\"" + item.value + "\"]");
+	      if (inputs.length == 1) {
+	        var input = inputs[0];
+	        val = input.automateValue || input.afValue || input.value;
+	      } else if (inputs.length > 1 && inputs[0].type == "radio") {
+	        for (var i = 0; i < inputs.length; i++) {
+	          if (inputs[i].checked) {
+	            val = inputs[i].value;
+	            break;
+	          }
+	        }
+	      }
 	    } else {
 	      val = data.get(item.value);
 	    }
 	    if (typeof val != "object") {
-	      if (!!parseFloat(val) || parseFloat(val) == 0) {
+	      if (val.match(/^\d+$/) && (!!parseFloat(val) || parseFloat(val) == 0)) {
 	        val = parseFloat(val);
 	      }
 	      output[item.value] = val;
